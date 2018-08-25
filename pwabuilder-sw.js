72f94484c5efb920487d30a498ee73c42dbd99ef
Jakeh9777 <jacobhaskin@gmail.com>
2018-08-25 17:20:18 -0500

    Update
    8-25-18
    5:20pm
    iOS PWA Update

--- pwabuilder-sw.js
+++ pwabuilder-sw.js
@@ -41,3 +41,39 @@ self.addEventListener('fetch', function(event) {
     })
   );
 })
+
+// Detects if device is on iOS 
+const isIos = () => {
+  const userAgent = window.navigator.userAgent.toLowerCase();
+  return /iphone|ipad|ipod/.test( userAgent );
+}
+// Detects if device is in standalone mode
+const isInStandaloneMode = () => ('standalone' in window.navigator) && (window.navigator.standalone);
+
+// Checks if should display install popup notification:
+if (isIos() && !isInStandaloneMode()) {
+  this.setState({ showInstallMessage: true });
+}
+
+import { ConnectedRouter, push } from 'react-router-redux';
+
+class PersistedConnectedRouter extends ConnectedRouter {
+  componentWillMount() {
+    const { store: propsStore, history, isSSR } = this.props;
+    this.store = propsStore || this.context.store;
+
+    if (!isSSR) {
+      this.unsubscribeFromHistory = history.listen(this.handleLocationChange);
+    }
+
+    //this is the tweak which will prefer persisted route instead of that in url:
+    const location = this.store.getState().router.location || {};
+    if (location.pathname !== history.location.pathname) {
+      this.store.dispatch(push(location.pathname));
+    }
+    this.handleLocationChange(history.location);
+    // --
+  }
+}
+
+export default PersistedConnectedRouter;
