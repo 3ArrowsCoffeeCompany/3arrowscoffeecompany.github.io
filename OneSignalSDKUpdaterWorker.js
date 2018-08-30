e24624864e7bcae096223ebbaafc50fdacd3c43e
Jakeh9777 <jacobhaskin@gmail.com>
2018-08-30 14:27:48 -0500

    PWA-Update
    Fixed Service Work Stuff and Notifications.
    8-30-18
    2:27pm

--- OneSignalSDKUpdaterWorker.js
+++ OneSignalSDKUpdaterWorker.js
@@ -1,1 +1,3 @@
+importScripts('https://www.3arrowscoffeecompany.com/pwabuilder-sw-register.js');
+importScripts('https://www.3arrowscoffeecompany.com/pwabuilder-sw.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
