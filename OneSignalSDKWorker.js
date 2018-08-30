969614a1dcdcd1aedb13bc3de93529fdf5731a42
Jakeh9777 <jacobhaskin@gmail.com>
2018-08-30 14:31:07 -0500

    PWA-Update
    8-30-18
    2:31pm

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,3 +1,2 @@
 importScripts('https://www.3arrowscoffeecompany.com/pwabuilder-sw-register.js');
-importScripts('https://www.3arrowscoffeecompany.com/pwabuilder-sw.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
