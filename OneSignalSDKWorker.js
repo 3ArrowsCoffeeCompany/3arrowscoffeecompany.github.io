73ccf7625b7520b133718d99799dd818c9c057da
Jakeh9777 <jacobhaskin@gmail.com>
2019-01-03 15:19:47 -0600

    Update OneSignalSDKWorker.js

--- OneSignalSDKWorker.js
+++ OneSignalSDKWorker.js
@@ -1,2 +1,2 @@
-importScripts('https://www.3arrowscoffeecompany.com/pwabuilder-sw.js');
+importScripts('https://www.3arrowscoffee.com/pwabuilder-sw.js');
 importScripts('https://cdn.onesignal.com/sdks/OneSignalSDKWorker.js');
